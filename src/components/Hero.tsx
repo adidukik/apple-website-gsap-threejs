214758c60f38b79454e77e2ec5101d3ef48e41ad
Damir Zhumatayev <adidukik@gmail.com>
2024-06-25 00:24:27 +0900

    install gsap

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,9 @@
 export default function Hero() {
-  return <div>Hero</div>;
+  return (
+    <section className='w-full nav-height bg-black relative'>
+      <div className='h-5/6 w-full flex-center flex-col'>
+        <p className='hero-title'>iPhone 15 Pro</p>
+      </div>
+    </section>
+  );
 }
