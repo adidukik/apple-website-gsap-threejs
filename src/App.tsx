c8999ea8f5dffd591a0b08d975e4689ebeb224c4
Damir Zhumatayev <adidukik@gmail.com>
2024-06-25 02:25:38 +0900

    install threejs

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,14 @@
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Highlights from './components/Highlights';
-
+import Model from './components/Model';
 export default function App() {
   return (
     <main className='bg-black'>
-      {/* todo!() : add the following components */}
-      {/* Navbar */}
-      {/* Hero */}
-      {/* Highlights */}
       <Navbar />
       <Hero />
       <Highlights />
+      <Model />
     </main>
   );
 }
