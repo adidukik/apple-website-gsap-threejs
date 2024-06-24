80e7491d2326787a9f075c66c5041bd85c50feb3
Damir Zhumatayev <adidukik@gmail.com>
2024-06-25 00:54:22 +0900

    add resizable video

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,32 @@
 import gsap from 'gsap';
 import { useGSAP } from '@gsap/react';
+import { heroVideo, smallHeroVideo } from '../utils';
+import { useEffect, useState } from 'react';
 
 export default function Hero() {
+  const [videoSrc, setVideoSrc] = useState(
+    window.innerWidth < 768 ? smallHeroVideo : heroVideo
+  );
+
+  const handleVideoSrcSet = () => {
+    if (window.innerWidth < 768) {
+      setVideoSrc(smallHeroVideo);
+    } else {
+      setVideoSrc(heroVideo);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener('resize', handleVideoSrcSet);
+    return () => {
+      window.removeEventListener('resize', handleVideoSrcSet);
+    };
+  }, []);
+
   // initially opacity is zero, and useGSAP will make it 1
   useGSAP(() => {
     gsap.to('#hero', {
       opacity: 1,
-      delay: 1.0,
+      delay: 1.2,
     });
   }, []);
 
@@ -17,6 +37,17 @@ export default function Hero() {
         <p id='hero' className='hero-title'>
           iPhone 15 Pro
         </p>
+        <div className='md:w-10/12 w-9/12'>
+          <video
+            className='pointer-events-none'
+            autoPlay
+            muted
+            playsInline={true}
+            key={videoSrc}
+          >
+            <source src={videoSrc} type='video/mp4' />
+          </video>
+        </div>
       </div>
     </section>
   );
