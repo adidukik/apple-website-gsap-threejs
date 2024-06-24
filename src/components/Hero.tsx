cb83a125a46fa38e96d5ed2f4a8599d5fa004240
Damir Zhumatayev <adidukik@gmail.com>
2024-06-25 01:09:13 +0900

    add call to action

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -26,7 +26,13 @@ export default function Hero() {
   useGSAP(() => {
     gsap.to('#hero', {
       opacity: 1,
-      delay: 1.2,
+      delay: 2,
+    });
+
+    gsap.to('#cta', {
+      opacity: 1,
+      y: -50,
+      delay: 2,
     });
   }, []);
 
@@ -48,6 +54,16 @@ export default function Hero() {
             <source src={videoSrc} type='video/mp4' />
           </video>
         </div>
+      </div>
+
+      <div
+        id='cta'
+        className='flex flex-col items-center opacity-0 translate-y-20'
+      >
+        <a href='#highlights' className='btn'>
+          Buy
+        </a>
+        <p className='font-semibold text-xl'>From $199/month or $999</p>
       </div>
     </section>
   );
