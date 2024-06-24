4df53d55750e1d1f5e1b4c770abec48158250aa8
Damir Zhumatayev <adidukik@gmail.com>
2024-06-25 00:30:11 +0900

    animate hero p using gsap

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,8 +1,22 @@
+import gsap from 'gsap';
+import { useGSAP } from '@gsap/react';
+
 export default function Hero() {
+  // initially opacity is zero, and useGSAP will make it 1
+  useGSAP(() => {
+    gsap.to('#hero', {
+      opacity: 1,
+      delay: 1.0,
+    });
+  }, []);
+
   return (
+    // nav-height specifies that opacity is 0 initially
     <section className='w-full nav-height bg-black relative'>
       <div className='h-5/6 w-full flex-center flex-col'>
-        <p className='hero-title'>iPhone 15 Pro</p>
+        <p id='hero' className='hero-title'>
+          iPhone 15 Pro
+        </p>
       </div>
     </section>
   );
