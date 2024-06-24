5b5e48333691c64d0b6e39e810cb188394526f84
Damir Zhumatayev <adidukik@gmail.com>
2024-06-25 00:18:08 +0900

    add navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,24 @@
 import { appleImg, bagImg, searchImg } from '../utils';
+import { navLists } from '../constants';
 
 export default function Navbar() {
   return (
-    <header>
-      <nav>
+    <header className='flex w-full py-5 sm:px-10 px-5 justify-between items-center'>
+      <nav className='flex w-full screen-max-width'>
         <img src={appleImg} alt='Apple logo' width={14} height={18} />
-        <div>
-          {['iPhones', 'MacBooks', 'iPads'].map((nav) => (
-            <div key={nav}>{nav}</div>
+        {/* navLists = ['Store', 'Mac', 'iPad', 'iPhone', 'Watch', 'Support']; */}
+        <div className='flex flex-1 justify-center max-sm:hidden'>
+          {navLists.map((nav) => (
+            <div
+              key={nav}
+              className='px-5 text-sm cursor-pointer text-gray-200 hover:text-white transition-all'
+            >
+              {nav}
+            </div>
           ))}
         </div>
-
-        <div>
+        {/* icons */}
+        <div className='flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1'>
           <img src={searchImg} alt='search' width={18} height={18} />
           <img src={bagImg} alt='bag' width={18} height={18} />
         </div>
