84ef7aab6f05c01773feb95e55536a5972416a6b
Damir Zhumatayev <adidukik@gmail.com>
2024-06-24 23:56:52 +0900

    Project setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,10 @@
 export default function App() {
-  return <h1 className='text-3xl font-bold underline'>Hello world!</h1>;
+  return (
+    <main className='bg-black'>
+      {/* todo!() : add the following components */}
+      {/* Navbar */}
+      {/* Hero */}
+      {/* Highlights */}
+    </main>
+  );
 }
